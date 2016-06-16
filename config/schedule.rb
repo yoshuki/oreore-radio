473b89e8cc4f6eb78ca561ce5df6476320496369
MIKAMI Yoshiyuki <yoshuki@saikyoline.jp>
2016-06-16 15:40:45 +0900

    dd "nichiyou" to schedules

--- config/schedule.rb
+++ config/schedule.rb
@@ -62,3 +62,7 @@ every(:saturday, at: '01:10') { send rake_method, 'oreore:podcast' }
 # tokyopod
 every(:sunday, at: '04:05') { send rake_method, 'oreore:import_from_ripdiko' }
 every(:sunday, at: '04:10') { send rake_method, 'oreore:podcast' }
+
+# nichiyou
+every(:sunday, at: '17:05') { send rake_method, 'oreore:import_from_ripdiko' }
+every(:sunday, at: '17:10') { send rake_method, 'oreore:podcast' }
