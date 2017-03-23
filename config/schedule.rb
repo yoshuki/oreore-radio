433e6e15dcc2c9199ca313ce41b6670f73fc6df4
MIKAMI Yoshiyuki <yoshuki@saikyoline.jp>
2017-03-23 14:38:26 +0900

    Add new denpa schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -62,3 +62,7 @@ every(:sunday, at: '12:05') { send rake_method, 'oreore:podcast' }
 # nichiyou
 every(:sunday, at: '17:05') { send rake_method, 'oreore:import_from_ripdiko' }
 every(:sunday, at: '17:10') { send rake_method, 'oreore:podcast' }
+
+# denpa
+every(:sunday, at: '21:00') { send rake_method, 'oreore:import_from_ripdiko' }
+every(:sunday, at: '21:05') { send rake_method, 'oreore:podcast' }
