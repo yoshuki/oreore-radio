85752b6899811a22dc50571a0d88742f5fd26c98
MIKAMI Yoshiyuki <yoshuki@saikyoline.jp>
2017-10-08 22:36:20 +0900

    Add dsk

--- config/schedule.rb
+++ config/schedule.rb
@@ -63,6 +63,10 @@ every(:sunday, at: '12:05') { send rake_method, 'oreore:podcast' }
 every(:sunday, at: '17:05') { send rake_method, 'oreore:import_from_ripdiko' }
 every(:sunday, at: '17:10') { send rake_method, 'oreore:podcast' }
 
+# dsk
+every(:sunday, at: '18:05') { send rake_method, 'oreore:import_from_ripdiko' }
+every(:sunday, at: '18:10') { send rake_method, 'oreore:podcast' }
+
 # yose
 every(:sunday, at: '21:00') { send rake_method, 'oreore:import_from_ripdiko' }
 every(:sunday, at: '21:05') { send rake_method, 'oreore:podcast' }
