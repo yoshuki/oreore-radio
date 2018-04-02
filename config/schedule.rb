6216fcfb9f1868b35c6952aaa946163a800b7296
MIKAMI Yoshiyuki <yoshuki@saikyoline.jp>
2018-04-02 21:32:09 +0900

    Update schedules

--- config/schedule.rb
+++ config/schedule.rb
@@ -43,10 +43,6 @@ end
 every('35 15 * * 1-5') { send rake_method, 'oreore:import_from_ripdiko' }
 every('40 15 * * 1-5') { send rake_method, 'oreore:podcast' }
 
-# kazoku
-every('05 22 * * 2-5') { send rake_method, 'oreore:import_from_ripdiko' }
-every('10 22 * * 2-5') { send rake_method, 'oreore:podcast' }
-
 # midnight
 every('05 1 * * 2-6') { send rake_method, 'oreore:import_from_ripdiko' }
 every('10 1 * * 2-6') { send rake_method, 'oreore:podcast' }
@@ -78,3 +74,15 @@ every(:sunday, at: '18:10') { send rake_method, 'oreore:podcast' }
 # yose
 every(:sunday, at: '21:00') { send rake_method, 'oreore:import_from_ripdiko' }
 every(:sunday, at: '21:05') { send rake_method, 'oreore:podcast' }
+
+# tmp1
+every(:tuesday, at: '21:35') { send rake_method, 'oreore:import_from_ripdiko' }
+every(:tuesday, at: '21:40') { send rake_method, 'oreore:podcast' }
+
+# tmp2
+every(:wednesday, at: '21:35') { send rake_method, 'oreore:import_from_ripdiko' }
+every(:wednesday, at: '21:40') { send rake_method, 'oreore:podcast' }
+
+# tmp3
+every(:friday, at: '22:05') { send rake_method, 'oreore:import_from_ripdiko' }
+every(:friday, at: '22:10') { send rake_method, 'oreore:podcast' }
